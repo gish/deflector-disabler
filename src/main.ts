75516f5e338de3e7abde7cac0921a5eaab3b2a02
Erik Hedberg <erik@hedberg.at>
2024-06-22 19:30:53 +0200

    synchronize the pulsar containment

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,6 @@ import { XMLParser } from "fast-xml-parser";
 import { Podcast } from "podcast";
 import fs from "node:fs";
 
-const FEED_INPUT_URL =
-  "https://api.sr.se/api/v2/episodes/index?programid=2071&fromdate=2024-06-01&todate=2024-12-31&audioquality=hi";
-
-const getFeed = async (url: string): Promise<string | null> => {
-  const response = await fetch(url);
-  if (!response.ok) {
-    return null;
-  }
-  return await response.text();
-};
-
 type SRFeed = {
   sr: {
     copyright: string;
@@ -37,6 +26,17 @@ type SRFeedEpisode = {
     url: string;
     publishdateutc: string;
   };
+};
+
+const FEED_INPUT_URL =
+  "https://api.sr.se/api/v2/episodes/index?programid=2071&fromdate=2024-06-01&todate=2024-12-31&audioquality=hi";
+
+const getFeed = async (url: string): Promise<string | null> => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    return null;
+  }
+  return await response.text();
 };
 
 const parseFeed = (XMLData: string): SRFeed | null => {
@@ -82,11 +82,13 @@ const main = async () => {
     console.log("Failed getting feed");
     process.exit(1);
   }
+
   const parsedFeed = parseFeed(feed);
   if (!parsedFeed) {
     console.log("Failed parsing feed");
     process.exit(1);
   }
+
   const podcastFeed = generatePodcastFeed(parsedFeed);
   const writeFileFeedResult = writeFileFeed(podcastFeed);
   if (!writeFileFeedResult) {
