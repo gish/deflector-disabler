7abc8c5aef1bd5ff57683a09d1ff3183e6567bff
Erik Hedberg <erik@hedberg.at>
2024-06-30 15:24:14 +0200

    initialize the dark matter regulator

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,19 @@
 import { getFeedEpisodes } from "./main";
 import fs from "node:fs";
 
-describe("episode list", () => {
-  const fixture = fs.readFileSync("./tests/fixtures/feed.twoItems.xml");
+type TestCase = { name: string; file: string };
+
+const testCases: TestCase[] = [
+  { name: "Two items", file: "feed.twoItems.xml" },
+  { name: "One item", file: "feed.oneItem.xml" },
+];
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
+test.each(testCases)("$name", async ({ file }) => {
+  const filePath = `./tests/fixtures/${file}`;
+  const fixture = fs.readFileSync(filePath);
   global.fetch = jest.fn(() =>
     Promise.resolve({
       text: () => Promise.resolve(fixture),
@@ -10,12 +21,6 @@ describe("episode list", () => {
     })
   ) as jest.Mock;
 
-  afterEach(() => {
-    jest.restoreAllMocks();
-  });
-
-  it("generates episode list", async () => {
-    const episodes = await getFeedEpisodes("fake.url");
-    expect(episodes).toMatchSnapshot();
-  });
+  const episodes = await getFeedEpisodes("fake.url");
+  expect(episodes).toMatchSnapshot();
 });
