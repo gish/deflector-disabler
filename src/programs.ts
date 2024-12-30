a73f24cf3335baeb2b24810f9a15cf0fecfff34a
Erik Hedberg <erik@hedberg.at>
2024-12-30 19:37:31 +0100

    add new cover for sommar vinter p1

--- src/programs.ts
+++ src/programs.ts
@@ -8,7 +8,8 @@ export const programs: RadioProgram[] = [
     slug: "avsnitt-av-sommar-vinter-i-p1",
     description:
       "Avsnitt av Sommar i P1 från 2024. Allt innehåll kommer från Sveriges Radio.",
-    imageUrl: "https://gish.github.io/deflector-disabler/assets/cover.png",
+    imageUrl:
+      "https://gish.github.io/deflector-disabler/assets/avsnitt-av-sommar-vinter-i-p1-cover.jpg",
     lastUpdated: new Date("2024-06-01").getTime(),
   },
   {
