4fa426af00b0ba9262e06acf20e13ef31040d6e6
Erik Hedberg <erik@hedberg.at>
2025-01-06 08:35:51 +0100

    update sommar description

--- src/programs.ts
+++ src/programs.ts
@@ -7,7 +7,7 @@ export const programs: RadioProgram[] = [
     title: "Avsnitt av Sommar & Vinter i P1",
     slug: "avsnitt-av-sommar-vinter-i-p1",
     description:
-      "Avsnitt av Sommar i P1 från 2024. Allt innehåll kommer från Sveriges Radio.",
+      "Avsnitt av Sommar & Vinter i P1 från 2024 och framåt. Allt innehåll kommer från Sveriges Radio.",
     imageUrl:
       "https://gish.github.io/deflector-disabler/assets/avsnitt-av-sommar-vinter-i-p1-cover.jpg",
     lastUpdated: new Date("2024-06-01").getTime(),
