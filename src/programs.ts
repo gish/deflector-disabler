563ad3d88d1e64350e33af1b193676273a81b28b
Erik Hedberg <erik@hedberg.at>
2026-01-10 20:53:10 +0100

    Add P3 Dokumentar

--- src/programs.ts
+++ src/programs.ts
@@ -3,17 +3,23 @@ import { Program } from "./types";
 export const programs: Program[] = [
   {
     srId: 2071,
-    title: "Avsnitt av Sommar & Vinter i P1",
+    title: "Sommar & Vinter i P1",
     lastUpdated: new Date("2024-06-01").getTime(),
   },
   {
     srId: 5067,
-    title: "Avsnitt av P3 Historia",
+    title: "P3 Historia",
     lastUpdated: new Date("2024-12-01").getTime(),
   },
   {
     srId: 4923,
-    title: "Avsnitt av USAPodden",
+    title: "USAPodden",
+    lastUpdated: new Date("2025-04-01").getTime(),
+  },
+  {
+    srId: 2519,
+    title: "P3 Dokumentär",
+
     lastUpdated: new Date("2025-04-01").getTime(),
   },
 ];
